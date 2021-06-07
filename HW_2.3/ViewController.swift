407f00f5fe67222eaf37dabdfda619a33ce12908
MikeO <mike@gmail.com>
2021-06-07 20:39:38 +0700

    change design

--- HW_2.3/ViewController.swift
+++ HW_2.3/ViewController.swift
@@ -7,8 +7,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+    //MARK: - IB Outlets
+class LoginViewController: UIViewController {
+    @IBOutlet var userNameTextFiled: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    //MARK: - Private properies
+    private let user = "User"
+    private let password = "Password"
+    
+    //MARK: - Navigation
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
